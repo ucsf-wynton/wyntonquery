4e8184aa16fab39d9a7f854609f47800851eeb2b
Henrik Bengtsson <hb@aroma-project.org>
2021-10-15 14:13:58 -0700

    BUG FIX: Removed argument 'auto_close' from open_file_at(); not safe

--- R/open_file_at.R
+++ R/open_file_at.R
@@ -9,26 +9,15 @@
 #' @param open (character) Mode to open the file connection in, cf.
 #' [base::file()].
 #'
-#' @param auto_close (logical) If TRUE, then the connection is automatically
-#' closed when garbage collected.
-#'
 #' @return A file [base::connection] moved to position `offset`.
 #'
 #' @importFrom utils file_test
 #' @export
-open_file_at <- function(file, offset = 0, open = "rb", auto_close = TRUE) {
+open_file_at <- function(file, offset = 0, open = "rb") {
   stopifnot(file_test("-f", file))
   file_size <- file.size(file)
   stopifnot(is.numeric(offset), offset >= 0, offset <= file_size)
   con <- file(file, open = open)
-  if (auto_close) {
-    env <- new.env()
-    env$con <- con
-    attr(con, "gcMe") <- env
-    reg.finalizer(env, function(e) {
-      try(close(e$con), silent = TRUE)
-    })
-  }
   if (offset > 0L) seek(con, where = offset, origin = "start")
   con
 }
